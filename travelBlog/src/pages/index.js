2b31930bb39865e9968776e161b3b6d916cfd4a1
kristy-peris <e3534529@student.dcccd.edu>
2020-11-30 17:57:50 -0600

    init

---
+++ travelBlog/src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Welcome to my Adventure! Feel free to explore!</h1>
+    <h2>My name is Kristy and this is where I log my adventures</h2>
+    <p>Need a Travel Agent? <Link to="/contact">Click Here!</Link></p>
+  </Layout>
+)
+
+export default IndexPage
